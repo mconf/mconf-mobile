8fb726521df6587910f9b1eebacacf4402d03dae
Felipe Cecagno <fcecagno@gmail.com>
2011-02-03 13:29:18 -0200

    some modifications on header

--- bbb-java/src/org/mconf/bbb/IBigBlueButtonClientListener.java
+++ bbb-java/src/org/mconf/bbb/IBigBlueButtonClientListener.java
@@ -1,18 +1,22 @@
 /*
- * This file is part of MConf-Mobile.
+ * GT-Mconf: Multiconference system for interoperable web and mobile
+ * http://www.inf.ufrgs.br/prav/gtmconf
+ * PRAV Labs - UFRGS
+ * 
+ * This file is part of Mconf-Mobile.
  *
- * MConf-Mobile is free software: you can redistribute it and/or modify
+ * Mconf-Mobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MConf-Mobile is distributed in the hope that it will be useful,
+ * Mconf-Mobile is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with MConf-Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Mconf-Mobile.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.mconf.bbb;
