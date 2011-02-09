3dec6fb27e97d7795c8872edb9107b2912975083
Alessandra <ale.lionheart@gmail.com>
2011-02-09 15:38:43 -0200

    started implementing the sliding in the private chat; autoscroll on the chat; removed all strings from the xml ant put on strings.xml

--- bbb-android/src/org/mconf/bbb/android/PrivateChat.java
+++ bbb-android/src/org/mconf/bbb/android/PrivateChat.java
@@ -22,6 +22,7 @@
 package org.mconf.bbb.android;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -35,8 +36,13 @@ import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -45,6 +51,7 @@ import android.widget.ViewFlipper;
 
 public class PrivateChat extends Activity {
 
+	
 	private class RemoteParticipant implements IBigBlueButtonClientListener {
 		private int userId;
 		private int viewId;
@@ -121,7 +128,15 @@ public class PrivateChat extends Activity {
 
 	// userId x remote participant
 	protected static Map<Integer, RemoteParticipant> participants = new HashMap<Integer, RemoteParticipant>();
-
+	
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 400;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+	
+	private GestureDetector gestureDetector;
+	View.OnTouchListener gestureListener;
+	private Animation slideLeft;
+	private Animation slideRight;
 	private ViewFlipper flipper;
 
 	private int addView() {
@@ -134,6 +149,18 @@ public class PrivateChat extends Activity {
 		return getLayoutInflater().inflate(R.layout.chat, null);        
 	}
 	
+	private RemoteParticipant getParticipantByViewId(int viewId) {
+		Iterator<Integer> it = participants.keySet().iterator(); 
+		while(it.hasNext()) { 
+			Object key = it.next();  
+			RemoteParticipant part = participants.get(key);
+			log.debug("view id: " + viewId + "partId: " + part.getViewId());
+			if(viewId==part.getViewId())
+				return part;
+		}
+		return null;
+	}
+	
 	private RemoteParticipant createParticipant(int userId, String username) {
 		log.debug("creating a new remote participant");
 		
@@ -143,10 +170,25 @@ public class PrivateChat extends Activity {
 		p.setViewId(addView());
 		p.setChatAdapter(new ChatAdapter(this));
 		
+		slideLeft = AnimationUtils.loadAnimation(this, R.anim.slide_left);
+        slideRight = AnimationUtils.loadAnimation(this, R.anim.slide_right);
+
+        gestureDetector = new GestureDetector(new MyGestureDetector());
+        gestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (gestureDetector.onTouchEvent(event)) {
+                    return true;
+                }
+                return false;
+            }
+        };
+		
 		final ListView chatListView = (ListView) flipper.getChildAt(p.getViewId()).findViewById(R.id.messages);
 		chatListView.setAdapter(p.getChatAdapter());
 		Client.bbb.addListener(p);
 
+		
+		
 		Button send = (Button) flipper.getChildAt(p.getViewId()).findViewById(R.id.sendMessage);
 		send.setOnClickListener(new OnClickListener() {
 
@@ -190,12 +232,12 @@ public class PrivateChat extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.private_chat);
 
+		
 		log.debug("creating first PrivateChat activity");
 
 		flipper = (ViewFlipper) findViewById(R.id.manyPages); 
-//		flipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.push_left_in)); 
-//		flipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.push_left_out));
-
+		
+        
 		displayView(getIntent().getExtras());
 	}
 	
@@ -214,6 +256,45 @@ public class PrivateChat extends Activity {
 		displayView(intent.getExtras());
 	}
 	 
+	
+	class MyGestureDetector extends SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                int viewID;
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	flipper.setInAnimation(slideLeft);
+                	flipper.setOutAnimation(slideLeft);
+                	viewID =flipper.getChildAt(flipper.getDisplayedChild() + 1).getId();
+                    
+                    setTitle(getParticipantByViewId(viewID).getUsername());
+            
+                    flipper.showNext();
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	flipper.setInAnimation(slideRight);
+                	flipper.setOutAnimation(slideRight);
+                	viewID =flipper.getChildAt(flipper.getDisplayedChild() -1).getId();
+                   System.out.println("viewId: " + viewID);
+                    	setTitle(getParticipantByViewId(viewID).getUsername());
+                    
+                	flipper.showPrevious();
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+    }
+	@Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event))
+	        return true;
+	    else
+	    	return false;
+    }
 }
 
 
