76ab22fdcdd49511c9e36945e088418972fa5c92
Felipe Cecagno <fcecagno@gmail.com>
2011-03-31 15:47:02 -0300

    removing unusable files

--- bbb-android/jni/iva/decode/copyToDeps.bat
+++
@@ -1,4 +0,0 @@
-copy /Y /B *.h ..\..\deps\prav\include\
-copy /Y /B Debug\decoded.lib ..\..\deps\prav\lib\
-copy /Y /B Debug\decoded.pdb ..\..\deps\prav\lib\
-copy /Y /B Release\decode.lib ..\..\deps\prav\lib\
